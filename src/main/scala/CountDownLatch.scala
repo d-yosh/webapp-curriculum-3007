0e371d5c27a19d2f49d17140aa23aad69e22f709
d-yosh <daicedaice87@gmail.com>
2020-01-20 11:06:48 +0900

    CountDownLatchSample を実装

--- src/main/scala/CountDownLatch.scala
+++ src/main/scala/CountDownLatch.scala
@@ -1,3 +1,36 @@
+import java.util.concurrent.atomic.AtomicInteger
+
+import scala.concurrent.{Future, Promise}
+import scala.util.Random
+import scala.concurrent.ExecutionContext.Implicits.global
+
 object CountDownLatchSample extends App {
-  ???
+  val random = Random
+  val maxWaitMillis = 1000
+  val counter = new AtomicInteger(0)
+  val promises = for (i <- 1 to 3) yield Promise[Int]
+  val futures = for (i <- 1 to 8)
+    yield
+      Future[Int] {
+        val waitMilliSec = random.nextInt(maxWaitMillis)
+        Thread.sleep(waitMilliSec)
+        waitMilliSec
+      }
+
+  futures.foreach { f =>
+    f.foreach {
+      case waitMilliSec =>
+        val index = counter.getAndIncrement
+        if (index < promises.length) {
+          promises(index).success(waitMilliSec)
+        }
+    }
+  }
+  promises.foreach {
+    p => p.future.foreach {
+      case waitMilliSec => println(waitMilliSec)
+    }
+  }
+
+  Thread.sleep(5000)
 }
